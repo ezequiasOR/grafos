0119a8bfe73cf88fb889d9cb1df2f93895a0623c
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2019-04-13 09:28:44 -0300

    Adicionando o JGraphT ao projeto

--- EP1/src/exercicioPratico1/Questao1.java
+++ EP1/src/exercicioPratico1/Questao1.java
@@ -1,5 +1,24 @@
 package exercicioPratico1;
 
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+import org.jgrapht.io.CSVFormat;
+
 public class Questao1 {
-	
+	public static void main(String[] args) {
+
+		Graph<String, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
+
+		graph = MyJGraphTUtil.importGraphCSV(
+				graph, 
+				"./src/main/java/graphs/5-3regular.csv",
+				CSVFormat.MATRIX, 
+				false,
+				false,
+				true); // MATRIX_FORMAT_NODEID
+		
+		System.out.println("Vertices: " + graph.vertexSet());
+		System.out.println("Arestas: " + graph.edgeSet());
+	}
 }
