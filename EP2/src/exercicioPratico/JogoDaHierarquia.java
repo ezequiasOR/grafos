975628599c532dc3c3fa6f3cedcbc7328160f7d7
Papagalli <joao.vitor.souza@ccc.ufcg.edu.br>
2019-05-10 22:57:01 -0300

    Finalizando o EP2

--- EP2/src/exercicioPratico/JogoDaHierarquia.java
+++ EP2/src/exercicioPratico/JogoDaHierarquia.java
@@ -26,24 +26,27 @@ import org.jgrapht.traverse.RandomWalkIterator;
 public class JogoDaHierarquia {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		SimpleGraph<String, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
-		graph = (SimpleGraph<String, DefaultEdge>) importGraphCSV(graph, "./files/grafo.csv", CSVFormat.MATRIX, false, false, true);
-		
-		RandomWalkIterator <String, DefaultEdge> random = new RandomWalkIterator<>(graph);
-		String root = random.next();
-		
-		DefaultDirectedGraph<String, DefaultEdge> rootedTree = new DefaultDirectedGraph<>(DefaultEdge.class);
-		
-		getRootedTree(graph, root, rootedTree);
-		
-		int n = Integer.parseInt(sc.nextLine());   // Lendo a qunatidade de chaces que que o jogador tem
+		graph = (SimpleGraph<String, DefaultEdge>) importGraphCSV(graph, "./files/grafo.csv", CSVFormat.MATRIX, false,
+				false, true);
 		
 		if (GraphTests.isTree(graph)) {
+			RandomWalkIterator<String, DefaultEdge> random = new RandomWalkIterator<>(graph);
+			String root = random.next();
+
+			DefaultDirectedGraph<String, DefaultEdge> rootedTree = new DefaultDirectedGraph<>(DefaultEdge.class);
+
+			getRootedTree(graph, root, rootedTree);
+
+			System.out.print("Numero de chances: ");
+			int n = Integer.parseInt(sc.nextLine()); // Lendo a qunatidade de chaces que que o jogador tem
+
 			int i = 0;
 			boolean guard = true;
-			
+
 			while (guard && i < n) {
+				System.out.print("Vertice: ");
 				String guessing = sc.nextLine();
 				if (guessing.equals(root)) {
 					guard = false;
@@ -52,44 +55,64 @@ public class JogoDaHierarquia {
 					if (guard) {
 						List<String> pai = Graphs.predecessorListOf(rootedTree, guessing);
 						List<String> filhos = Graphs.successorListOf(rootedTree, guessing);
-						System.out.println(guessing + " nao eh raiz. O pai de " + guessing + " eh " + pai + " e os filhos de " + guessing + " sao " + filhos);
+						System.out.println(guessing + " nao eh raiz. O pai de " + guessing + " eh " + pai
+								+ " e os filhos de " + guessing + " sao " + filhos);
 					}
 				}
 				i++;
 			}
-			if (guard) { System.out.println("Numero de tentativas excedido!"); }
-			
+			if (guard) {
+				System.out.println("Numero de tentativas excedido!");
+			}
+
 		}
 		
+		else {
+			
+			System.out.println("O Grafo nao eh uma arvore!");
+			
+		}
+
 		sc.close();
 	}
-	
-	public static <V,E> int level (Graph <V,E> g, V root, V v) {
-		if (g.containsVertex(v)&&root.equals(v)) { return 0; }
-		else {
-			DijkstraShortestPath <V,E> pfinder = new DijkstraShortestPath <> (g);
-			return pfinder.getPath(root,v).getLength();
+
+	public static <V, E> int level(Graph<V, E> g, V root, V v) {
+		if (g.containsVertex(v) && root.equals(v)) {
+			return 0;
+		} else {
+			DijkstraShortestPath<V, E> pfinder = new DijkstraShortestPath<>(g);
+			return pfinder.getPath(root, v).getLength();
 		}
 	}
 	
-	public static void getRootedTree (SimpleGraph <String,DefaultEdge> basegraph, String root, DefaultDirectedGraph <String,DefaultEdge> rt) {
+	
+	/**
+	 * O Método transformara uma arvore em um grafo direcionado com uma raiz (Uma arvore direcionada).
+	 * @param basegraph Arvore base.
+	 * @param root Raiz desejada.
+	 * @param rt Grafo direcionado base, que sera alterado.
+	 */
+	public static void getRootedTree(SimpleGraph<String, DefaultEdge> basegraph, String root,
+			DefaultDirectedGraph<String, DefaultEdge> rt) {
 		Graphs.addAllVertices(rt, basegraph.vertexSet());
-		Iterator <DefaultEdge> it = basegraph.edgeSet().iterator();
+		Iterator<DefaultEdge> it = basegraph.edgeSet().iterator();
 		while (it.hasNext()) {
 			DefaultEdge e = it.next();
 			String source = basegraph.getEdgeSource(e);
 			String target = basegraph.getEdgeTarget(e);
-			if (level(basegraph,root,source) > level(basegraph,root,target)) {
+			if (level(basegraph, root, source) > level(basegraph, root, target)) {
 				rt.addEdge(target, source, new DefaultEdge());
-			} else rt.addEdge(source, target,new DefaultEdge());
-		}	
+			} else
+				rt.addEdge(source, target, new DefaultEdge());
+		}
 	}
-	
+
 	/**
 	 * O Metodo importara um arquivo CSV e o transformara em um objeto grafo.
-	 * @param graph Grafo que sera utilizado como base.
+	 * 
+	 * @param graph    Grafo que sera utilizado como base.
 	 * @param filename Nome do arquivo CSV.
-	 * @param f Formato do arquivo.
+	 * @param f        Formato do arquivo.
 	 * @return O Grafo com base no arquivo CSV recebido.
 	 */
 	public static Graph<String, DefaultEdge> importGraphCSV(Graph<String, DefaultEdge> graph, String filename,
@@ -110,9 +133,10 @@ public class JogoDaHierarquia {
 
 	/**
 	 * O Metodo importara um arquivo CSV e o transformara em um objeto grafo.
-	 * @param graph Grafo que sera utilizado como base.
-	 * @param filename Nome do arquivo CSV.
-	 * @param f Formato do arquivo.
+	 * 
+	 * @param graph                            Grafo que sera utilizado como base.
+	 * @param filename                         Nome do arquivo CSV.
+	 * @param f                                Formato do arquivo.
 	 * @param pMATRIX_FORMAT_ZERO_WHEN_NO_EDGE
 	 * @param pEDGE_WEIGHT
 	 * @param pMATRIX_FORMAT_NODEID
@@ -137,9 +161,10 @@ public class JogoDaHierarquia {
 		}
 		return graph;
 	}
-	
+
 	/**
 	 * Ira ler o arquivo CSV.
+	 * 
 	 * @param filename Nome do arquivo.
 	 * @return Retornara um Reader.
 	 */
@@ -156,24 +181,26 @@ public class JogoDaHierarquia {
 		StringReader readergml = new StringReader(contentBuilder.toString());
 		return readergml;
 	}
-	
+
 	/**
 	 * Printara um grafo a partir de seus vertices e arestas.
+	 * 
 	 * @param g Grafo a ser printado.
 	 */
-	public static <V,E> void printGraph (Graph <V,E> g ) {
-        System.out.println(g.vertexSet());
-		System.out.println(g.edgeSet()+"\n");
+	public static <V, E> void printGraph(Graph<V, E> g) {
+		System.out.println(g.vertexSet());
+		System.out.println(g.edgeSet() + "\n");
 	}
-	
+
 	/**
 	 * Printara um grafo a partir de seus vertices e arestas.
-	 * @param g Grafo a ser printado.
+	 * 
+	 * @param g     Grafo a ser printado.
 	 * @param title Titulo do grafo.
 	 */
-	public static <V,E> void printGraph (Graph <V,E> g, String title ) {
+	public static <V, E> void printGraph(Graph<V, E> g, String title) {
 		System.out.println(title);
-        System.out.println(g.vertexSet());
-		System.out.println(g.edgeSet()+"\n");
+		System.out.println(g.vertexSet());
+		System.out.println(g.edgeSet() + "\n");
 	}
 }
