2583dc1d89c8a7a096d381339fc19e53be9f4e3a
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2019-06-04 20:45:46 -0300

    Lendo arquivo gml

--- EP3/src/exercicioPratico/Questao.java
+++ EP3/src/exercicioPratico/Questao.java
@@ -17,15 +17,16 @@ import org.jgrapht.io.VertexProvider;
 
 public class Questao {
 	public static void main(String[] args) {
-		Graph<String, DefaultEdge> ugraph = new Multigraph<>(DefaultEdge.class);
-		importGraphGML(ugraph, "./src/main/java/graphs/email-Eu-core.gml");
+		Graph<DefaultVertex, RelationshipEdge> graph = new Multigraph<>(RelationshipEdge.class);
+		importGraphGML(graph, "./files/email-Eu-core-parte2.gml");
+		printGraph(graph);
 	}
 	
-	public static Graph<String,DefaultEdge> importGraphGML (Graph<String,DefaultEdge> graph, String filename) {
-		VertexProvider<String> vp1 = (label, attributes) -> new String(label, attributes);
-		EdgeProvider<String, DefaultEdge> ep1 = (from, to, label, attributes) -> new RelationshipEdge(from,
+	public static Graph<DefaultVertex,RelationshipEdge> importGraphGML (Graph<DefaultVertex,RelationshipEdge> graph, String filename) {
+		VertexProvider<DefaultVertex> vp1 = (label, attributes) -> new DefaultVertex(label, attributes);
+		EdgeProvider<DefaultVertex, RelationshipEdge> ep1 = (from, to, label, attributes) -> new RelationshipEdge(from,
 				to, attributes);
-		GmlImporter<String, DefaultEdge> gmlImporter = new GmlImporter<>(vp1, ep1);
+		GmlImporter<DefaultVertex, RelationshipEdge> gmlImporter = new GmlImporter<>(vp1, ep1);
 		try {
 			gmlImporter.importGraph(graph, readFile(filename));
 		} catch (ImportException e) {
@@ -34,7 +35,7 @@ public class Questao {
 		return graph;
 	}
 	
-	static Reader readFile(String filename) {
+    static Reader readFile(String filename) {
 		StringBuilder contentBuilder = new StringBuilder();
 		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
 			String sCurrentLine;
@@ -46,5 +47,16 @@ public class Questao {
 		}
 		StringReader readergml = new StringReader(contentBuilder.toString());
 		return readergml;
+	}
+    
+    public static <V,E> void printGraph (Graph <V,E> g ) {
+        System.out.println(g.vertexSet());
+		System.out.println(g.edgeSet()+"\n");
+	}
+	
+	public static <V,E> void printGraph (Graph <V,E> g, String title ) {
+		System.out.println(title);
+        System.out.println(g.vertexSet());
+		System.out.println(g.edgeSet()+"\n");
 	}
 }
